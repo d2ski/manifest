9aa12ac98deba832365e0bbe10ba471c6e08d1b5
d2ski <chukotski@gmail.com>
2024-07-22 09:13:38 +0200

    add tailwind and daisyui

--- src/app/manifest/manifest.component.ts
+++ src/app/manifest/manifest.component.ts
@@ -6,7 +6,6 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './manifest.component.html',
-  styleUrl: './manifest.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ManifestComponent {}
